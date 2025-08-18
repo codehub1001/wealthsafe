8baf4ad15f61dba5571c89422541b0c77020639a
chuka <oluwachukkie@gmail.com>
2025-08-18 15:11:51 +0100

    Inl commit

--- src/components/TawkToWidget.jsx
+++ src/components/TawkToWidget.jsx
@@ -5,17 +5,21 @@ const TawkToWidget = () => {
   useEffect(() => {
     if (window.Tawk_API) return; // Prevent duplicate script injection
 
-    const s1 = document.createElement('script');
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/6891b6fdddd4a01926700de1/1j1sj5kf9';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
+    const Tawk_API = window.Tawk_API || {};
+    const Tawk_LoadStart = new Date();
 
-    const s0 = document.getElementsByTagName('script')[0];
-    s0.parentNode.insertBefore(s1, s0);
+    (function () {
+      const s1 = document.createElement('script');
+      const s0 = document.getElementsByTagName('script')[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/68a3337875e23f192b2c4ced/1j2uo24fq';
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+    })();
   }, []);
 
-  return null; // No visible UI, it's just injecting the script
+  return null; // No visible UI, just injects the script
 };
 
 export default TawkToWidget;
